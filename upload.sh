86975ea54fe599f2bfaece87d9cbccc40ef87884
mac <mac@zz-rdmac1590.local>
2019-10-22 10:43:40 +0800

    初始化版本#2

---
+++ upload.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VersionString=`grep -E 's.version.*=' WZPDatePicker.podspec`
+VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
+
+NewVersionNumber=$(($VersionNumber + 1))
+LineNumber=`grep -nE 's.version.*=' WZPDatePicker.podspec | cut -d : -f1`
+sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" WZPDatePicker.podspec
+
+echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
+
+git add .
+git commit -am '初始化版本'#${NewVersionNumber}
+git tag ${NewVersionNumber}
+git push origin master --tags
+pod repo push WZPRepoSpecs WZPDatePicker.podspec --verbose --allow-warnings --use-libraries --use-modular-headers
+
